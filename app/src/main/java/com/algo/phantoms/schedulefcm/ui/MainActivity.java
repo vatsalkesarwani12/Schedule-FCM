652acf984367374878552ad0bf481472deada2ed
plazzy99 <vatsalkesarwani12@gmail.com>
2021-03-12 20:37:50 +0530

    server side notification done

--- app/src/main/java/com/algo/phantoms/schedulefcm/ui/MainActivity.java
+++ app/src/main/java/com/algo/phantoms/schedulefcm/ui/MainActivity.java
@@ -1,6 +1,8 @@
 package com.algo.phantoms.schedulefcm.ui;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,27 +12,37 @@ import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseMessaging.getInstance()
-                .subscribeToTopic("offers")
-                .addOnCompleteListener(task -> {
-                    Toast.makeText(
-                            MainActivity.this,
-                            "Subscribed! You will get all offers notifications",
-                            Toast.LENGTH_SHORT)
-                            .show();
+        findViewById(R.id.buttonSubscribe).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Subscribing to offers");
 
-                    if (!task.isSuccessful()) {
-                        Toast.makeText(
-                                MainActivity.this,
-                                "Failed! Try again.",
-                                Toast.LENGTH_SHORT)
-                                .show();
-                    }
-                });
+                FirebaseMessaging.getInstance()
+                        .subscribeToTopic("offers")
+                        .addOnCompleteListener(task -> {
+                            Toast.makeText(
+                                    MainActivity.this,
+                                    "Subscribed! You will get all offers notifications",
+                                    Toast.LENGTH_SHORT)
+                                    .show();
+
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(
+                                        MainActivity.this,
+                                        "Failed! Try again.",
+                                        Toast.LENGTH_SHORT)
+                                        .show();
+                            }
+                        });
+
+            }
+        });
     }
 }
